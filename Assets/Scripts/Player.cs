94d044d9f1c8dab7c88e7d384d470132da084993
Goncalo Nascimento <goncalonascimento.gamedesign@gmail.com>
2025-06-02 04:29:46 +0100

    Some changes in player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,16 +2,30 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Vector2 initialVelocity;
+    private Vector2 velocity;
+    private CoinScript coinScript;
+    private int howManyCoins;
 
     void Start()
     {
+        howManyCoins = coinScript.coinStach;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-
-    }
+        velocity = rb.linearVelocity;
+        float dir = Input.GetAxis("Horizontal");
+        if (howManyCoins != 0)
+        {
+            velocity.x = velocity.x / howManyCoins;
+        }
+ 
+        Debug.Log(velocity.x);
+        rb.linearVelocity = velocity;
+    }   
 
 }
